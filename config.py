739bc6ed44bac39de0b0ca6a8bb11541e77c4a2a
PortNum <57895822+PortNum@users.noreply.github.com>
2023-07-06 21:53:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-url = "https://open.feishu.cn/open-apis/bot/v2/hook/57dfbb83-ca88-42f1-81e5-89e75eb6b2eb"
-secret_string = 'C2YJrXQC5uN4I3zW6VwTAc'
+url = ""
+secret_string = ''
 headers = {"Content-Type": "application/json"}
 
 db_name = os.path.join(os.getcwd(), "notify.db")
